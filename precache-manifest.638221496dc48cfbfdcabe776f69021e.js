24d6997cb10b1fd28f698fb55ab8a926ec9d6c08
Kanghwan Lee <kanghwan89@gmail.com>
2020-01-17 09:52:36 +0900

    Updates

---
+++ precache-manifest.638221496dc48cfbfdcabe776f69021e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0de545f4cdd845578b80b9b53861cbb",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8c7296a977f2dfc12802",
+    "url": "/movie_app/static/css/main.cd155b3e.chunk.css"
+  },
+  {
+    "revision": "9530636b5f37038ba74f",
+    "url": "/movie_app/static/js/2.e88d2649.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.e88d2649.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c7296a977f2dfc12802",
+    "url": "/movie_app/static/js/main.4282b383.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
